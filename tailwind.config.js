96bb9aa64071897d1b90046982a7e7d615c592d9
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-05 12:10:23 +0330

    edit tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,35 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode:"class",
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        "brown": {
+          "100": "#ECE0D1",
+          "300": "#DBC1AC",
+          "600": "#967259",
+          "900": "#634832"
+        }
+      }
     },
+
+    boxShadow: {
+      "light": "0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
+      "dark": " 0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
+    },
+
+    // borderRadius: {
+    //   "4xl": "2rem",
+    // },
+    fontFamily: {
+      "Dana": "Dana",
+      "DanaMedium": "Dana Medium",
+      "DanaDemiBold": "Dana DemiBold",
+      "MorabbaLight": "Morabba light",
+      "MorabbaMedium": "Morabba Medium",
+      "MorabbaBold": "Morabba Bold",
+    }
   },
   plugins: [],
 };
