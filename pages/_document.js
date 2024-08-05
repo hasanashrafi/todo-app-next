76a8100d442173b2e47315588606cd0b65819ddb
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-05 12:13:30 +0330

    edit html and body styles

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="fa" dir="rtl">
       <Head />
-      <body>
+      <body className="bg-gradient-to-t from-[#5d0efa] to-[#ebe7ff]">
         <Main />
         <NextScript />
       </body>
