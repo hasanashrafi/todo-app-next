a1314806b68b8b5692079195f517eb78d09b1ab2
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-05 12:12:41 +0330

    create user model with mongoose

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model, models } = require("mongoose");
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "Email is required"],
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Email is invalid",
+        ],
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        match: [
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,128}$)/,
+        ]
+    },
+    todos: [{
+        title: String,
+        status: String
+    }],
+    createdAt: {
+        type: Date,
+        default: () => Date.now(),
+        immutable: true,
+    }
+})
+
+const User = models.User || model("User", userSchema)
+
+export default User
