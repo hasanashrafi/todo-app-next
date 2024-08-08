ee2cd3b3519859f6c25fdb50825926c8694fc4af
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-08 14:26:34 +0330

    create radio button component

---
+++ components/element/RadioButton.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+
+function RadioButton({ status, setStatus, value, title, children }) {
+
+    return (
+        <div className='flex gap-x-3'>
+
+            <div className=' relative rounded-md shadow-sm'>
+                <div className={`flex gap-x-2 border w-fit p-2 rounded-md  ${value === "todo" ? "bg-green-600" : null
+                        || value === "inprogress" ? "bg-blue-600 " : null
+                            || value === "review" ? "bg-yellow-600 " : null
+                                || value === "done" ? "bg-violet-600 " : null}`}>
+
+                    <label htmlFor={value}
+                        className="flex gap-x-2 text-sm font-medium leading-4  text-white">
+                        {children}
+                        {title}
+                    </label>
+                    <input
+                        onChange={e => setStatus(e.target.value)}
+                        type='radio'
+                        id='todo'
+                        value={value}
+                        checked={status === value}
+                        className=" cursor-pointer"
+                    />
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default RadioButton;
