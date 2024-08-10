6a8bf13e8be2e0e61aec508f9c970f288a31f7a6
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-10 10:50:26 +0330

    wrap session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
-
-export default function App({ Component, pageProps }) {
+import { SessionProvider } from "next-auth/react"
+export default function App({ 
+  Component, pageProps }) {
   return (
  
-
+<SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+</SessionProvider>
  
   )
 }
