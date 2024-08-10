bac7291a7c8513a9948a4264ae6b6eef5c25627b
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-10 10:46:09 +0330

    create nextauth function with credentials

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+import { verifyPassword } from "@/utils/auth";
+
+
+
+const authOptions = {
+    session: { strategy: "jwt" },
+   
+        providers: [
+            CredentialsProvider({
+                async authorize(credentials, req) {
+                    const { email, password } = credentials;
+
+                    try {
+                        await connectDB()
+                    } catch (error) {
+                        throw new Error("Error in connect DB")
+                    }
+
+                    if (!email || !password) throw new Error("Enter Email and password")
+
+                    const user = await User.findOne({ email: email })
+                    if (!user) throw new Error("User doesn't Exist")
+
+                    const isValid = await verifyPassword(password, user.password)
+                    if (!isValid) throw new Error("Email or password incorrect")
+
+                    return { email }
+                },
+            }),
+        ],
+       
+    }
+
+
+
+export default NextAuth(authOptions)
