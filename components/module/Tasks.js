1c41ea213cefc3cbef72c1bfd88644d0ecdc9d85
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-11 20:02:48 +0330

    styles for span

--- components/module/Tasks.js
+++ components/module/Tasks.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { RiMastodonLine } from "react-icons/ri";
 
 function Tasks({ data }) {
     console.log(data)
@@ -6,8 +7,15 @@ function Tasks({ data }) {
         <div className='flex flex-col gap-y-2 p-3 items-center justify-center  rounded '>
             {
                 data?.map((i) => (
-                    <div key={i._id} className='size-24 border bg-white rounded text-black'>
-                        <span className={i.status}>{i.title}</span>
+                    <div key={i._id} className='flex flex-col w-full p-3 border bg-white rounded text-black'>
+                        <span className={
+                            `w-full h-2 rounded mb-3 ${i.status === "todo" ? "bg-green-600" : null
+                                || i.status === "inprogress" ? "bg-blue-600" : null
+                                    || i.status === "review" ? "bg-yellow-600" : null
+                                        || i.status === "done" ? "bg-violet-600" : null}`
+                        }></span>
+                        <p className=''>{i.title}</p>
+                        <RiMastodonLine className='self-end text-xl' />
                     </div>
                 ))
             }
