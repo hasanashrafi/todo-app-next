065d4cf95e96167645c26254b0f5b24c60550f5c
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-12 16:40:17 +0330

    remove logs

--- components/module/ProfileInfo.js
+++ components/module/ProfileInfo.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Layout from '../layout/Layout'
 
 function ProfileInfo({ profileData }) {
-    console.log(profileData)
+  
     return (
         <Layout>
             <div className='min-h-screen '>
