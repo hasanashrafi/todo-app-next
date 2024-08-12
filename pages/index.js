9201f9185e1c544a843c77c82466d900e5538146
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-12 16:41:39 +0330

    remove logs and white spaces

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,35 @@
 import Layout from "@/components/layout/Layout";
 import HomePage from "@/components/templates/HomePage";
+import { getSession } from "next-auth/react";
 
 
 export default function Home() {
-  
-  return (
-<Layout>
 
-    <main
-      className={`min-h-screen p-4 `}
-    >
-      <div className="flex  rounded justify-center  h-full font-mono text-sm lg:flex">
-        <HomePage/>
-      </div>
-    </main>
- 
-</Layout>
-);
+  return (
+    <Layout>
+      <main
+        className={`min-h-screen p-4 `}
+      >
+        <div className="flex  rounded justify-center  h-full font-mono text-sm lg:flex">
+          <HomePage />
+        </div>
+      </main>
+
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req })
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      }
+    }
+  }
+  return {
+    props: {}
+  }
 }
