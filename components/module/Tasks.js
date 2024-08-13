0c6e0b6074f41417a90936997adea560606664e8
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-13 23:30:23 +0330

    refactor codes

--- components/module/Tasks.js
+++ components/module/Tasks.js
@@ -1,35 +1,26 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react'
+import React, { useState } from 'react';
+import { CgLogIn } from 'react-icons/cg';
 import { FcTodoList } from "react-icons/fc";
 import { GrFormNextLink } from "react-icons/gr";
 
-
 function Tasks({ data, next, back, fetchTodos }) {
-   
     const changeStatus = async (id, status) => {
         const res = await fetch("/api/todos", {
             method: "PATCH",
             body: JSON.stringify({ id, status }),
             headers: { "Content-Type": "application/json" }
-        })
-        const data = await res.json()
-        if (data.status === "success") fetchTodos()
-    }
-
-    const detailHandler = (id) => {
-    
-        fetch(`/api/todos/${id}`)
-            .then((res) => res.json())
-            .then((data) => console.log(data.data))
-
-    }
+        });
+        const data = await res.json();
+        if (data.status === "success") fetchTodos();
+    };
 
     return (
-        <div className='flex flex-col gap-y-2 p-3 items-center justify-center  rounded '>
+        <div className='flex flex-col gap-y-2 p-3 items-center justify-center rounded'>
             {
                 data?.map((i) => (
-                    <div key={i._id} className='flex flex-col w-full p-3 border bg-white rounded text-black'>
+                    <div key={i._id} className='flex flex-col w-full p-3 border bg-white rounded text-black transform transition-transform duration-300 hover:scale-105'>
                         <span className={
                             `w-[70%] mx-auto h-1 rounded mb-3 ${i.status === "todo" ? "bg-green-600" : null
                                 || i.status === "inprogress" ? "bg-blue-600" : null
@@ -37,18 +28,20 @@ function Tasks({ data, next, back, fetchTodos }) {
                                         || i.status === "done" ? "bg-violet-600" : null}`
                         }>
                         </span>
-                        <button onClick={() => detailHandler(i._id)}>Detail</button>
-                        <div className='flex justify-between items-center my-4 border p-2 rounded bg-slate-200'>
-                            <p className=''>{i.title}</p>
-                            <FcTodoList className='self-end text-xl' />
-                        </div>
+                        <Link href={`/detail/${i._id}`}>
+                            <div className='flex justify-between items-center my-4 border p-2 rounded bg-slate-200'>
+                                <p className=''>{i.title}</p>
+                                <FcTodoList className='self-end text-xl' />
+                            </div>
+                        </Link>
+
                         <div className='flex justify-between my-2.5 items-center p-1'>
                             {
                                 back ? (
                                     <button
                                         onClick={() => changeStatus(i._id, back)}
-                                        className="flex gap-x-1 items-center  bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white p-1  border border-red-500 hover:border-transparent rounded">
-                                        <GrFormNextLink className=' text-xl  ' />
+                                        className="flex gap-x-1 items-center bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white p-1 border border-red-500 hover:border-transparent rounded">
+                                        <GrFormNextLink className='text-xl' />
                                         قبلی
                                     </button>
                                 ) : null
@@ -57,10 +50,9 @@ function Tasks({ data, next, back, fetchTodos }) {
                                 next ? (
                                     <button
                                         onClick={() => changeStatus(i._id, next)}
-
-                                        className="flex gap-x-1 items-center   bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white p-1 border border-blue-500 hover:border-transparent rounded">
+                                        className="flex gap-x-1 items-center bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white p-1 border border-blue-500 hover:border-transparent rounded">
                                         بعدی
-                                        <GrFormNextLink className='text-xl rotate-180 ' />
+                                        <GrFormNextLink className='text-xl rotate-180' />
                                     </button>
                                 ) : null
                             }
@@ -69,7 +61,7 @@ function Tasks({ data, next, back, fetchTodos }) {
                 ))
             }
         </div>
-    )
+    );
 }
 
-export default Tasks
+export default Tasks;
