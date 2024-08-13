afb76b07e32a64e9b6f098eedd14f20ab3a1da6b
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-13 23:29:09 +0330

    edit styles

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,70 +1,62 @@
-import Link from 'next/link'
+import Link from 'next/link';
 import { VscListSelection } from "react-icons/vsc";
 import { BiMessageSquareAdd } from "react-icons/bi";
 import { RxDashboard } from "react-icons/rx";
 import Dropdown from '../templates/DropDown';
 
-
 function Layout({ children }) {
-
     return (
         <div className='font-Dana p-2'>
-            <header className='flex justify-between items-center  h-24  px-2.5 text-white text-lg'>
-                <div className='flex flex-col items-center justify-center  '>
-                    <p className="flex flex-col text-lg sm:text-2xl font-DanaDemiBold text-[#5115c2]">
+            <header className=' top-0 z-10 flex justify-between items-center h-18 px-2.5  text-violet-700 text-lg'>
+                <div className='flex flex-col items-center justify-center'>
+                    <p className="flex flex-col text-lg sm:text-2xl font-DanaDemiBold">
                         فهرست کارها
-                        <span className='text-[10px] text-center  font-Dana  text-[#726c7e]'>موفقیت : نظم , تلاش , تمرین</span>
-
+                        <span className='text-[10px] text-center font-Dana text-[#7244cf]'>موفقیت : نظم , تلاش , تمرین</span>
                     </p>
                 </div>
-
                 <div className='self-center p-1 text-[12px] lg:text-xl'>
                     <Dropdown />
                 </div>
             </header>
 
             <div className='flex'>
-                <section className='pb-16 flex  w-full'>
-                    <div className=' w-full'>
+                <section className='pb-16 flex w-full'>
+                    <div className='w-full'>
                         {children}
                     </div>
                 </section>
 
-                <div className='backdrop-filter backdrop-blur-md bg-opacity-20 bg-clip-padding fixed  bottom-10 right-0 left-0 w-[95%] sm:w-[55%] bg-[#5115c2] items-center flex justify-center mx-auto mb-5 self-end h-12  rounded-full  p-2'>
-
-                    <ul className='flex justify-around  items-center '>
-                        <li
-                            className='hover:bg-indigo-500 px-4 py-1 rounded-full text-sm lg:text-lg font-medium text-white my-2 flex items-center'>
-                            <VscListSelection className='lg:text-2xl' />
-                            <Link href="/" className='mr-2.5'>
-                                کار ها
-                            </Link>
+                <div className='fixed bottom-10 left-0 right-0 w-[95%] sm:w-[55%] bg-[#5115c2] backdrop-filter backdrop-blur-md bg-opacity-20 rounded-full p-2 mx-auto mb-5 flex justify-center'>
+                    <ul className='flex justify-around items-center'>
+                        <li className='hover:bg-indigo-500 px-4 py-1 rounded-full text-sm lg:text-lg font-medium text-white my-2 flex items-center'>
+                           
+                            <Link href="/" className='flex gap-x-1 items-center'>
+                             <VscListSelection className='lg:text-2xl' />
+                             
+                            کار ها 
+                             </Link>
                         </li>
-                        <li className='hover:bg-indigo-500 px-4 py-1 rounded-full text-sm lg:text-lg  font-medium text-white my-2 flex items-center'>
+                        <li className='hover:bg-indigo-500 px-4 py-1 rounded-full text-sm lg:text-lg font-medium text-white my-2 flex items-center'>
+                            <Link href="/add-todo" className='flex gap-x-1 items-center'>
                             <BiMessageSquareAdd className='lg:text-2xl' />
-
-                            <Link href="/add-todo" className='mr-2.5'>
-                                کار جدید
+                            کار جدید
                             </Link>
                         </li>
-                        <li className='hover:bg-indigo-500 px-4 py-1 rounded-full text-sm lg:text-lg  font-medium text-white my-2 flex items-center'>
-                            <RxDashboard className=' lg:text-2xl' />
-                            <Link href="/profile" className='mr-2.5'>
-                                پروفایل
+                        <li className='hover:bg-indigo-500 px-4 py-1 rounded-full text-sm lg:text-lg font-medium text-white my-2 flex items-center'>
+                            <Link href="/profile" className='flex gap-x-1 items-center'>
+                            <RxDashboard className='lg:text-2xl' />
+                            پروفایل
                             </Link>
                         </li>
                     </ul>
-
                 </div>
             </div>
 
-            <footer className=' p-3 items-center justify-center text-white font-MorabbaMedium flex  mx-auto w-full'><p>
-                Developed.By.HsN
-            </p>
+            <footer className='p-3 items-center justify-center text-white font-MorabbaMedium flex mx-auto w-full'>
+                <p>Developed.By.HsN</p>
             </footer>
-
         </div>
-    )
+    );
 }
 
-export default Layout
+export default Layout;
