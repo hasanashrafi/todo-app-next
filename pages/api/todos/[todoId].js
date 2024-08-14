bcffcdf5ff3f5fbe1c251f3cb36222a8072ebc7a
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-14 13:20:22 +0330

    added patch

--- pages/api/todos/[todoId].js
+++ pages/api/todos/[todoId].js
@@ -26,6 +26,34 @@ async function handler(req, res) {
             return res.status(401).json({ status: "failed", message: "Error in find" })
 
         }
+    } else if (req.method === "PATCH") {
+        const { title, status, description } = req.body;
+        const todoId = req.query.id;
+
+        if (!todoId || !title || !status) {
+            return res.status(422).json({ status: "failed", message: "Invalid Data!" });
+        }
+
+        const result = await User.updateOne(
+            { "todos._id": todoId },
+            {
+                $set: {
+                    "todos.$.status": status,
+                    "todos.$.description": description,
+                    "todos.$.title": title
+                },
+            }
+        );
+
+        if (result.nModified === 0) {
+            return res.status(404).json({ status: "failed", message: "Todo not found" });
+        }
+
+        return res.status(200).json({ status: "success", message: "Todo updated" });
+    } else {
+        return res.status(405).json({ status: "failed", message: "Method not allowed" });
     }
 }
+
+
 export default handler
